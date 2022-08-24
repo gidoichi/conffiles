b8c01d529d63af5d2041ddabb6d1c0145388daf8
Shinichi Yanagido <s.yanagido@gmail.com>
2022-08-24 21:13:26 +0900

    functionize

--- install.sh
+++ install.sh
@@ -2,6 +2,20 @@
 
 set -eu
 
+p() {
+    printf '%s' "$*"
+}
+
+# EXAMPLE
+# stdin(json-array): [{}]
+set_object_in_array_if_not_exists() {
+    key="$1"
+    val="$2"
+    p "${arr}" | jq -c '.[]' | while IFS= read -r sub; do
+        p "${sub}" | jq --arg k "${key}" --arg v "${val}" 'if .[$k] then . else . += {($k):$v} end'
+    done | jq -cs 'flatten(1)'
+}
+
 # EXAMPLE
 # $1(json-object): {"target":"t1.txt", "path":"/path/to/p1", "when":"[ 1 -eq 1 ]"}
 # stdout(json-array): [{"target":"t1.txt", "path":"/path/to/p1"}]
@@ -11,42 +25,33 @@ collect() (
     # base case at recursion
     arr='[{}]'
 
-    if cond=$(printf '%s' "${json}" | jq -ce '.cond'); then
+    if cond=$(p "${json}" | jq -ce '.cond'); then
         arr=$(
-            printf '%s' "${cond}" | jq -c '.[]' | while IFS= read -r sub; do
+            p "${cond}" | jq -c '.[]' | while IFS= read -r sub; do
                 collect "${sub}"
             done | jq -cs 'flatten(1)'
            )
     fi
 
-    if when=$(printf '%s' "${json}" | jq -er '.when'); then
+    if when=$(p "${json}" | jq -er '.when'); then
         sh -uc "${when}" || return
     fi
 
-    if target=$(printf '%s' "${json}" | jq -er '.target'); then
-        arr=$(
-            printf '%s' "${arr}" | jq -c '.[]' | while IFS= read -r sub; do
-                printf '%s' "${sub}" | jq --arg target "${target}" 'if .target then . else . += {"target":$target} end'
-            done | jq -cs 'flatten(1)'
-           )
+    if target=$(p "${json}" | jq -er '.target'); then
+        arr=$(p "${arr}" | set_object_in_array_if_not_exists 'target' "${target}")
     fi
-
-    if path=$(printf '%s' "${json}" | jq -er '.path'); then
+    if path=$(p "${json}" | jq -er '.path'); then
         path=$(sh -euc "printf '%s' \"${path}\"")
-        arr=$(
-            printf '%s' "${arr}" | jq -c '.[]' | while IFS= read -r sub; do
-                printf '%s' "${sub}" | jq --arg path "${path}" 'if .path then . else . += {"path":$path} end'
-            done | jq -cs 'flatten(1)'
-           )
+        arr=$(p "${arr}" | set_object_in_array_if_not_exists 'path' "${path}")
     fi
 
-    printf '%s' "${arr}"
+    p "${arr}"
 )
 
 list() (
     conf="$1"
 
-    printf '%s' "${conf}" | jq -c '.place[]' | while IFS= read -r json; do
+    p "${conf}" | jq -c '.place[]' | while IFS= read -r json; do
         collect "${json}"
     done
 
@@ -58,9 +63,9 @@ conf="${PARENT}/installation-place.yml"
 
 list=$(list "$(yq -o json "${conf}" | jq -c '.')")
 
-printf '%s' "${list}" | jq -c '.[]' | while IFS= read -r line; do
-    path=$(printf '%s' "${line}" | jq -r '.path')
-    target=$(printf '%s' "${line}" | jq -r '.target')
+p "${list}" | jq -c '.[]' | while IFS= read -r line; do
+    path=$(p "${line}" | jq -r '.path')
+    target=$(p "${line}" | jq -r '.target')
     # TODO: link file
     echo cp "${PARENT}/configs/${target}" "${path}${target}"
 done

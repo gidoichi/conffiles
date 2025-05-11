5400805d3b7f79feeea1d8c314a29356617a146e
gidoichi <hakuto1yds@gmail.com>
2025-05-11 17:30:42 +0900

    remove powershell path environment

--- configs/PowerShell/profile.ps1
+++ configs/PowerShell/profile.ps1
@@ -1,8 +1,3 @@
-# PATH environment variable
-$ENV:Path += ";$((Get-ChildItem 'C:\Program Files\Emacs\' -Depth 1 -Filter bin).FullName)"
-$ENV:Path += ";$HOME\AppData\Roaming\.emacs.d\bin"
-$ENV:Path += ';C:\Program Files\nodejs'
-
 # key bindings
 Import-Module PSReadLine
 Set-PSReadlineOption -EditMode Emacs
